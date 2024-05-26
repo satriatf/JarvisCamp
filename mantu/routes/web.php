2cb0f1918ed0036f79e487e8663892060c78ed48
satriatf <satriatf11@gmail.com>
2024-05-26 11:25:36 +0700

    Pertemuan 8 Tugas 8 Blade Template

--- mantu/routes/web.php
+++ mantu/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard.index');
 });
 
 // route untuk menampilkan halaman daftar tugas
